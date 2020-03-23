34c742e585c30ae5c166cd9f3a7330ce1ea32319
nick-snyder <35740943+nick-snyder@users.noreply.github.com>
2020-03-23 18:33:05 -0400

    Assignment Desc.

--- Recipe Resizer.py
+++ Recipe Resizer.py
@@ -1,3 +1,10 @@
+# Create a program for Ms. Vashaw that will change the yield output for a given recipe. 
+# For example, if the given ingredients in a recipe serves 4, 
+# the program should be able to increase the amounts in the recipe to (double it or triple it) 
+# or reduce the amounts (half it or quarter it). 
+# The user should be able to input original ingredient quantities and recipe’s yield, 
+# then using mathematical expressions, give the increase or decrease for that recipe based on what the user wants.
+
 whole_recipe = []
 ingredient_name = ""
 ingredient_ounces = ""
